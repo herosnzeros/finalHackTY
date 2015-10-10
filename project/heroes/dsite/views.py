9371d062d023d395ec585fa99bc7feb832f298b8
LilianVachkov <lilvachkov@gmail.com>
2015-10-10 14:47:24 +0300

    Ivan e batal

---
+++ project/heroes/dsite/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+import MySQLdb
+
+# Create your views here.
+def index(request):
+    return render(request, 'homepage.html', locals())
+
+def profile(request):
+    return render(request, 'profile.html', locals())
+
---
+++ project/heroes/heroes/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import patterns, include, url
+from django.contrib.auth.views import login
+from django.contrib import admin
+from dsite.views import index, profile
+admin.autodiscover()
+
+from dsite import views
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'heroes.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    # url(r'^', views.index),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', index),
+    url(r'^profile/$', profile),
+    url(r'^login/$', login , {"template_name":"login.html"}),
+)
