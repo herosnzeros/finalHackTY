a1c5ee5da7463a8c32589cb952d716896854b8c9
ivanmilevtues <ivanmilevtues@gmail.com>
2015-10-10 18:30:38 +0300

    Fixed few things

--- project/heroes/dsite/models.py
+++ project/heroes/dsite/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Meeting(models.Model):
+    name = models.CharField(max_length=30)
+    creator = models.ForeignKey(User, related_name="created")
+    guest = models.ForeignKey(User)
+
